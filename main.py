58080d0f01b309d4e104d6d8b41c90bec13b52d1
biteliang <45704863+biteliang@users.noreply.github.com>
2023-08-20 12:59:04 +0800

    Squash and merge (#1)
    * hello world
    
    * someting change
    
    * push my_feature branch
    
    ---------
    
    Co-authored-by: ljg-rkosn <45704863+ljg-rkosn@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # 初始化Git
 # git clone <https://xxxxxx>
 # git init
+
+# 新建分支,并在这个新分支上进行代码的修改
+# git checkout -b my_feature
+
+# 修改好保存分支
+# git add <file>
+# git commit -m "change"
+
+# 提交到远程仓库上
+# git push origin my_feature
+
+
+print("hello world")
